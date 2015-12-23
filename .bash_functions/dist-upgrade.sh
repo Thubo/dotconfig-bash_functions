dd7704ddbc6889c12d576306dbc40d57118cc8f4
Matthias Thubauville <matthias.thubo@gmail.com>
2015-12-23 20:11:53 +0100

    Update package lists only every 60 minutes

--- .bash_functions/dist-upgrade.sh
+++ .bash_functions/dist-upgrade.sh
@@ -2,9 +2,18 @@
 # dist-upgrade: update your os
 dist-upgrade()
 {
-  sudo echo 'Starting Update'
-  sudo apt-get -qqq update
+  minutes_since_last_update=$((($(date +%s)-$(stat -c %Y /var/lib/apt/periodic/update-success-stamp))/60))
+
+  if [[ $minutes_since_last_update -gt 60 ]]; then
+    echo -n "Getting package information..."
+    sudo apt-get -qqq update
+    echo "Done!"
+  else
+    echo "Assuming package information is up-to-date!"
+  fi
+
   sudo apt-get -y dist-upgrade
   sudo apt-get -y autoremove
   alert
+
 }
